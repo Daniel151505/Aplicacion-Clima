fac2c7ed9ee4ce03f4c41b98fd5b747510eb9752
Daniel151505 <daniel150520011@gmail.com>
2021-02-21 13:13:47 -0500

    Importando HttpClient

--- Aplicacion-Clima/src/app/services/clima.service.ts
+++ Aplicacion-Clima/src/app/services/clima.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -7,5 +8,5 @@ export class ClimaService {
   url = "api.openweathermap.org/data/2.5/weather?&appid="
   key="523bd798dd5a7ae5aaa0b7f1ad7399b6"
 
-  constructor() { }
+  constructor( private http: HttpClient) { }
 }
